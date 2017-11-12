f23bfe3aab1c1375435d098f7d73f22dc6171e33
Ethan Jaszewski <ethanjaszewski@yahoo.com>
2017-11-12 13:41:06 -0800

    Configured JavaFX & Gradle

---
+++ src/main/java/edu/caltech/parsec/teststand/ClientApp.java
@@ -0,0 +1,17 @@
+package edu.caltech.parsec.teststand;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ClientApp extends Application {
+    
+    public void start(Stage mainStage) {
+        
+        mainStage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ src/test/java/edu/caltech/parsec/teststand/ClientAppTest.java
@@ -0,0 +1,5 @@
+package edu.caltech.parsec.teststand;
+
+public class ClientAppTest {
+
+}
